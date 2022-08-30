d3fa2c9cfd5c0d0f1d2be03b44dab5de31758c01
coldrook <8768561+coldrook@users.noreply.github.com>
2022-08-30 22:01:29 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,14 +11,14 @@
 #
 
 ##补充汉化       
-cp -r -f ./feeds/xiangfeidexiaohuo/files/udpxy.lua ./feeds/luci/applications/luci-app-udpxy/luasrc/model/cbi
+#cp -r -f ./feeds/xiangfeidexiaohuo/files/udpxy.lua ./feeds/luci/applications/luci-app-udpxy/luasrc/model/cbi
 
 ##fixed mosdns
 rm -rf ./feeds/packages/net/mosdns
 cp -r -f ./feeds/xiangfeidexiaohuo/op-mosdns ./feeds/packages/net/mosdns
 
 
-# Modify default IP
+## Modify default IP
 sed -i 's/192.168.1.1/192.168.11.1/g' package/base-files/files/bin/config_generate
 
 ##加入作者信息
